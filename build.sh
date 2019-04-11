48830b035898734cb06bc763956cfce1d5984413
Kim Minsik <aqwe45696@gmail.com>
2019-04-11 21:33:34 +0900

    gcc -> clang

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir build
 
-g++ -Ofast -fopenmp -pthread -std=c++11 main.cpp -o build/main
-g++ -O3 -std=c++11 is_sorted.cpp -o build/is_sorted
+clang++ -Ofast -fopenmp -pthread -std=c++11 main.cpp -o build/main
+clang++ -O3 -std=c++11 is_sorted.cpp -o build/is_sorted
